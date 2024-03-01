47b0bff4f8b91a1c161c7e4bbab0420430d26590
Miguel Méndez Murias <miguelmendeztech@gmail.com>
2024-03-01 23:24:47 +0100

    FunctionType updated

--- Prácticas/Python--/src/ast/types/FunctionType.java
+++ Prácticas/Python--/src/ast/types/FunctionType.java
@@ -1,6 +1,7 @@
 package ast.types;
 
 import ast.Type;
+import ast.definitions.VariableDef;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,18 +9,18 @@ import java.util.List;
 
 public class FunctionType extends AbstractType  {
     private Type returnType;
-    private List<Type> params=new ArrayList<>();
-    public FunctionType(int line, int column, Type returnType, Type...params){
+    private List<VariableDef> params=new ArrayList<>();
+    public FunctionType(int line, int column, Type returnType, List<VariableDef> params){
         super(line, column);
         this.returnType=returnType;
-        this.params= Arrays.stream(params).toList();
+        this.params= params;
     }
 
     public Type getReturnType() {
         return returnType;
     }
 
-    public List<Type> getParams() {
+    public List<VariableDef> getParams() {
         return params;
     }
 }
