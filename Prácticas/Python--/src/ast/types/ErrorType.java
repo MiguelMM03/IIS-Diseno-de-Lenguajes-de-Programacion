18edcc0b5b93d03387a9f5d0e89cb56f4a8d04e5
Miguel Méndez Murias <miguelmendeztech@gmail.com>
2024-04-10 09:24:40 +0200

    ErrorType updated

--- Prácticas/Python--/src/ast/types/ErrorType.java
+++ Prácticas/Python--/src/ast/types/ErrorType.java
@@ -1,7 +1,11 @@
 package ast.types;
 
+import ast.ASTNode;
+import ast.Type;
 import errorhandler.ErrorHandler;
 import semantic.Visitor;
+
+import java.util.List;
 
 public class ErrorType extends AbstractType{
     private String message;
@@ -17,5 +21,55 @@ public class ErrorType extends AbstractType{
     @Override
     public <TP,TR> TR accept(Visitor<TP,TR> visitor, TP param){
         return visitor.visit(this,param);
+    }
+    @Override
+    public Type asBuiltInType(ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type asLogical(ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type arithmetic(Type type,ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type arithmetic(ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type logic(Type type,ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type logic(ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type promotesTo(Type type,ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type canBeCastTo(Type type,ASTNode ast){
+        return this;
+
+    }
+    @Override
+    public Type comparison(Type type,ASTNode ast){
+        return this;
+
+    }
+    @Override
+    public Type squareBrackets(Type type,ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type dot(String field,ASTNode ast){
+        return this;
+    }
+    @Override
+    public Type parenthesis(List<Type> types, ASTNode ast){
+        return this;
     }
 }
